12af5b963a70f2c132d16c7c51c94a950aa9487e
Kevin Zhao <kzhao@uchicago.edu>
2017-09-21 15:41:44 -0500

    .

--- pkgs/apps/blackscholes/src/blackscholes.c
+++ pkgs/apps/blackscholes/src/blackscholes.c
@@ -315,6 +315,7 @@ int bs_thread(void *tid_ptr) {
 #endif
             heartbeat(heart);
         }
+        printf("Iteration\n"):
     }
     printf("Thread done with heartrate %llu\n", heart->heartrate);
     heart_destroy(heart);
