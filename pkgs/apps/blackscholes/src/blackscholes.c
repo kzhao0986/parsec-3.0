23f29eecf3a049291a2051cb1f359492124f2a3c
Kevin Zhao <kzhao@uchicago.edu>
2017-09-25 19:52:42 -0500

    .

--- pkgs/apps/blackscholes/src/blackscholes.c
+++ pkgs/apps/blackscholes/src/blackscholes.c
@@ -291,8 +291,11 @@ int bs_thread(void *tid_ptr) {
 
     heart_init(heart, 20000, 0);
 
-    heart_setscheduler();
-    // deadline_setscheduler(30 * 1000 * 1000, 30 * 1000 * 1000);
+    if (getenv("SCHED_HEARTBEAT")) {
+        heart_setscheduler();
+    } else {
+        deadline_setscheduler(30 * 1000 * 1000, 30 * 1000 * 1000);
+    }
 
     for (j=0; j<NUM_RUNS; j++) {
 #ifdef ENABLE_OPENMP
