086c2a74fb855f6994c0c381c15e065c666daa34
Kevin Zhao <kzhao@uchicago.edu>
2017-10-10 21:21:11 -0500

    .

--- pkgs/apps/swaptions/src/HJM_Swaption_Blocking.cpp
+++ pkgs/apps/swaptions/src/HJM_Swaption_Blocking.cpp
@@ -149,8 +149,7 @@ int HJM_Swaption_Blocking(FTYPE *pdSwaptionPrice, //Output vector that will stor
   //Simulations begin:
   for (l=0;l<=lTrials-1;l+=BLOCKSIZE) {
       //For each trial a new HJM Path is generated
-      // iSuccess = HJM_SimPath_Forward_Blocking(ppdHJMPath, iN, iFactors, dYears, pdForward, pdTotalDrift,ppdFactors, &iRndSeed, BLOCKSIZE); /* GC: 51% of the time goes here */
-      return 1;
+      iSuccess = HJM_SimPath_Forward_Blocking(ppdHJMPath, iN, iFactors, dYears, pdForward, pdTotalDrift,ppdFactors, &iRndSeed, BLOCKSIZE); /* GC: 51% of the time goes here */
        if (iSuccess!=1)
 	return iSuccess;
       
