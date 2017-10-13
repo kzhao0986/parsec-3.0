42c41bfc621004b84c3d72f6ecadacc200b62967
Kevin Zhao <kzhao@uchicago.edu>
2017-10-13 14:03:48 -0500

    .

--- pkgs/apps/swaptions/src/HJM_Securities.cpp
+++ pkgs/apps/swaptions/src/HJM_Securities.cpp
@@ -124,7 +124,7 @@ void * worker(void *arg){
      // assert(iSuccess == 1);
      swaptions[i].dSimSwaptionMeanPrice = pdSwaptionPrice[0];
      swaptions[i].dSimSwaptionStdError = pdSwaptionPrice[1];
-     printf("Heartbeat.\n");
+     printf("Heartbeat. %d\n", pthread_self());
      heartbeat(heart);
    }
 
