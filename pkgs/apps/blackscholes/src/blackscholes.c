313267ab3baf5fe6f746baa9639d8f6bf4c73c8b
Kevin Zhao <kzhao@uchicago.edu>
2017-10-16 23:08:11 -0500

    .

--- pkgs/apps/blackscholes/src/blackscholes.c
+++ pkgs/apps/blackscholes/src/blackscholes.c
@@ -281,7 +281,7 @@ int bs_thread(void *tid_ptr) {
 DWORD WINAPI bs_thread(LPVOID tid_ptr){
 #else
 
-static const uint64_t targets[] = { 10000, 10000 };
+static const uint64_t targets[] = { 11000, 9000 };
 
 int bs_thread(void *tid_ptr) {
 #endif
