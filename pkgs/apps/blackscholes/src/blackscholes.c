bc8c79e5c03dc8022a27b6472021e18494e3dfdf
Kevin Zhao <kzhao@uchicago.edu>
2017-10-17 22:39:30 -0500

    .

--- pkgs/apps/blackscholes/src/blackscholes.c
+++ pkgs/apps/blackscholes/src/blackscholes.c
@@ -297,7 +297,7 @@ int bs_thread(void *tid_ptr) {
 
     params.schedtype = HEARTBEAT;
     params.target = targets[tid];
-    params.window = 100000;
+    params.window = 150000;
     params.runtime = 30 * 1000 * 1000;
     params.period = 30 * 1000 * 1000;
 
