293c249b746577f76e64ea1e5092db2ebdcda40f
Kevin Zhao <kzhao@uchicago.edu>
2017-11-03 19:31:03 -0500

    Try this

--- run_native.sh
+++ run_native.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 echo "" > /var/log/syslog 
+
 sync
-sudo $3 LD_LIBRARY_PATH=/usr/local/lib ./bin/parsecmgmt -c gcc-hooks -a run -p $1 -n 2 -i native
-cat /var/log/syslog | grep Heartbeat > $2
+
+sudo LD_LIBRARY_PATH=/usr/local/lib ./bin/parsecmgmt -c gcc-hooks -a run -p $1 -n 2 -i native
+
+# Dump log to [test].log
+cat /var/log/syslog | grep Heartbeat > $1.log
+# Isolate lines containing perf targets and CPU shares
+grep -E '(Targets|share)' $1.log > $1.tmp
+
+while read in
+do
+	res=${in##*Heartbeat: }
+	echo $res >> $1.results
+done < $1.tmp
+
+rm $1.tmp
