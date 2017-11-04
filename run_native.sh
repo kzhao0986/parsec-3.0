c73bf13f8c3a0af1010d357086ff8889c16622a5
Kevin Zhao <kzhao@uchicago.edu>
2017-11-03 21:09:38 -0500

    .

--- run_native.sh
+++ run_native.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 echo "" > /var/log/syslog 
-
 sync
 
-sudo LD_LIBRARY_PATH=/usr/local/lib ./bin/parsecmgmt -c gcc-hooks -a run -p $1 -n 2 -i native
+name=$1
+ratio=$2
+
+sudo $ratio LD_LIBRARY_PATH=/usr/local/lib ./bin/parsecmgmt \
+        -c gcc-hooks -a run -p $name -n 2 -i native
 
 # Dump log to [test].log
-cat /var/log/syslog | grep Heartbeat > $1.log
+cat /var/log/syslog | grep Heartbeat > $name.log
 # Isolate lines containing perf targets and CPU shares
-grep -E '(Targets|share)' $1.log > $1.tmp
+grep -E '(Targets|share)' $name.log > $name.tmp
 
+echo "$ratio to 1" >> $name.results
+echo "------------" >> $name.results
+
+# Put those lines into [test].results, pruning useless information.
 while read in
 do
 	res=${in##*Heartbeat: }
-	echo $res >> $1.results
-done < $1.tmp
+	echo $res >> $name.results
+done < $name.tmp
 
-rm $1.tmp
+echo "" >> $name.results
+
+rm $name.tmp
