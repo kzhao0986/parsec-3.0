69af4cf38380f2f2d49c09f4547c334afa85c77d
Kevin Zhao <kzhao@uchicago.edu>
2017-11-06 18:14:17 -0600

    .

--- run-native.sh
+++ run-native.sh
@@ -10,7 +10,7 @@ echo "$name: $ratio to 1..."
 
 sudo RATIO=$ratio LD_LIBRARY_PATH=/usr/local/lib ./bin/parsecmgmt \
      -c gcc-hooks -a run -p $name -n 2 -i native | \
-     grep iterations > $name.tmp 
+     grep iterations >> $name.tmp 
 
 # Dump log to [test].log
 cat /var/log/syslog | grep Heartbeat > $name.log
