58575054dca16cb777514746c5823a6c93934ee8
Kevin Zhao <kzhao@uchicago.edu>
2017-11-06 18:35:30 -0600

    .

--- run-native.sh
+++ run-native.sh
@@ -9,12 +9,13 @@ ratio=$2
 echo "$name: $ratio to 1..."
 
 sudo RATIO=$ratio LD_LIBRARY_PATH=/usr/local/lib ./bin/parsecmgmt \
-     -c gcc-hooks -a run -p $name -n 2 -i native | grep iterations
+     -c gcc-hooks -a run -p $name -n 2 -i native | \
+     grep iterations > $name.tmp
 
 # Dump log to [test].log
 cat /var/log/syslog | grep Heartbeat > $name.log
 # Isolate lines containing perf targets and CPU shares
-grep Targets $name.log > $name.tmp
+grep Targets $name.log >> $name.tmp
 
 echo "$ratio to 1" >> $name.results
 echo "------------" >> $name.results
