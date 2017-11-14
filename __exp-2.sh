690640eb85959149ec72cddb71e05b2a6f4f40f5
Kevin Zhao <kzhao@uchicago.edu>
2017-11-13 21:27:03 -0600

    .

--- __exp-2.sh
+++ __exp-2.sh
@@ -7,39 +7,41 @@ name=$1
 weights=$2
 schedtype=$3
 
-# echo "$name: $ratio to 1..."
+echo "$name: $weights"
 
 sudo exp_nr=2 weights="$weights" $schedtype LD_LIBRARY_PATH=/usr/local/lib \
      ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 4 -i native \
      #> /dev/null
 
-# cat /var/log/syslog | grep Heartbeat > $name.log
-# # Isolate lines containing perf targets and CPU shares
-# grep -E '(Targets|share)' $name.log > $name.tmp
+outfile=$name-exp2
 
-# echo "$ratio to 1" >> $name.results
-# echo "------------" >> $name.results
+cat /var/log/syslog | grep Heartbeat > $outfile.log
+# Isolate lines containing perf targets and CPU shares
+grep -E '(Targets|share)' $outfile.log > $outfile.tmp
 
-# # Copy [test].tmp into [test].results, pruning useless information.
-# while read in
-# do
-# 	res=${in##*Heartbeat: }
+echo "$ratio to 1" >> $outfile.results
+echo "------------" >> $outfile.results
 
-# 	# If this line contains CPU share information, it'll contain
-# 	# a big ugly fraction like "xxxxxxxxx/yyyyyyyyy". So let's
-# 	# do some extra formatting by piping that into `bc` and
-# 	# appending it to the end.
-# 	is_share=$(echo $res | grep share)
-# 	if [ -n "$is_share" ]
-# 	then
-# 		share_frac=${res##*share: } # x/y
-# 		share=$(echo "scale=3; $share_frac" | bc)
-# 		res="$res ($share)"
-# 	fi
+# Copy [test].tmp into [test].results, pruning useless information.
+while read in
+do
+	res=${in##*Heartbeat: }
 
-# 	echo $res >> $name.results
-# done < $name.tmp
+	# If this line contains CPU share information, it'll contain
+	# a big ugly fraction like "xxxxxxxxx/yyyyyyyyy". So let's
+	# do some extra formatting by piping that into `bc` and
+	# appending it to the end.
+	is_share=$(echo $res | grep share)
+	if [ -n "$is_share" ]
+	then
+		share_frac=${res##*share: } # x/y
+		share=$(echo "scale=3; $share_frac" | bc)
+		res="$res ($share)"
+	fi
 
-# echo "" >> $name.results
+	echo $res >> $outfile.results
+done < $outfile.tmp
 
-# rm $name.tmp
+echo "" >> $outfile.results
+
+rm $outfile.tmp
