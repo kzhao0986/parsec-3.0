5517bdea019e6b0d316395fa11e0e3c108ec0042
Kevin Zhao <kzhao@uchicago.edu>
2017-11-13 21:02:40 -0600

    .

--- __exp-2.sh
+++ __exp-2.sh
@@ -4,11 +4,12 @@ echo "" > /var/log/syslog
 sync
 
 name=$1
-schedtype=$2
+weights=$2
+schedtype=$3
 
 # echo "$name: $ratio to 1..."
 
-sudo exp_nr=2 weights="0.5 0.5 1 1" $schedtype LD_LIBRARY_PATH=/usr/local/lib \
+sudo exp_nr=2 weights=$weights $schedtype LD_LIBRARY_PATH=/usr/local/lib \
      ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 4 -i native \
      #> /dev/null
 
