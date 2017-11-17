0d1a956fb5d2bafacaf434c8107dbd129a9901fb
Kevin Zhao <kzhao@uchicago.edu>
2017-11-17 14:39:31 -0600

    .

--- __exp-1.sh
+++ __exp-1.sh
@@ -9,8 +9,8 @@ schedtype=$3
 
 echo "$name: $ratio to 1..."
 
-sudo EXP_NR=1 RATIO=$ratio $schedtype LD_LIBRARY_PATH=/usr/local/lib \
-     ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 1 -i native \
+sudo exp_nr=1 RATIO=$ratio $schedtype LD_LIBRARY_PATH=/usr/local/lib \
+     ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 2 -i native \
      #> /dev/null
 
 cat /var/log/syslog | grep Heartbeat > $name.log
