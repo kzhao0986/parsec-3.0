671ea7b0c655b0c174307c7accd26caadf8374da
Kevin Zhao <kzhao@uchicago.edu>
2017-11-17 21:03:22 -0600

    .

--- profile.sh
+++ profile.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 name=$1
-w1="0.5"
-w2="1"
+w1="0.5 0.5 0.5 0.5"
+w2="1 1 1 1"
 
 echo "Little Core"
-sudo exp_nr=2 weights=$w1 SCHED_HEARTBEAT=1 LD_LIBRARY_PATH=/usr/local/lib \
+sudo exp_nr=2 weights="$w1" SCHED_HEARTBEAT=1 \
      ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 1 -i native \
      #| grep "energy"
 
 echo ""
 
 echo "Big Core"
-sudo exp_nr=2 weights=$w2 SCHED_HEARTBEAT=1 LD_LIBRARY_PATH=/usr/local/lib \
+sudo exp_nr=2 weights="$w2" SCHED_HEARTBEAT=1 LD_LIBRARY_PATH=/usr/local/lib \
      ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 1 -i native \
      #| grep "energy"
