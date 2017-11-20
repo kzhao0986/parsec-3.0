2b1b25669a99373e707e015e2d2a6b16e774b058
Kevin Zhao <kzhao@uchicago.edu>
2017-11-19 22:12:18 -0600

    .

--- __exp-2.sh
+++ __exp-2.sh
@@ -8,8 +8,6 @@ weights=$2
 schedtype=$3
 suffix=$4
 
-echo $schedtype
-
 # Restrict the deadline scheduler to only use big cores. This way, we can
 # measure energy consumption given that targets can be met.
 if [ $schedtype == "SCHED_DEADLINE=1" ]
