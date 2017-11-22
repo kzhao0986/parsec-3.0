3b5a67369241e234a83585b6ac063a3c9a77ad90
Kevin Zhao <kzhao@uchicago.edu>
2017-11-22 14:19:50 -0600

    .

--- exp-2.sh
+++ exp-2.sh
@@ -14,7 +14,7 @@ fi
 #                     "0.5 1 1 1" \
 #                     "1 1 1 1")
 
-declare -a weights=("0.43 0.43 0.43 0.43")
+declare -a weights=("0.42 0.42 0.42 0.42")
 
 # get length of an array
 arraylength=${#weights[@]}
