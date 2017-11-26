8c1516dbfcae329f26d8eb3d3fd9d44f0eb13170
Kevin Zhao <kzhao@uchicago.edu>
2017-11-26 17:22:25 -0600

    .

--- exp-2.sh
+++ exp-2.sh
@@ -21,7 +21,7 @@ fi
 #                     "0.405 1 1 1" \
 #                     "1 1 1 1")
 
-declare -a weights=("0.25 0.25 0.25 0.25")
+declare -a weights=("0.24 0.24 0.24 0.24")
 
 # get length of an array
 arraylength=${#weights[@]}
@@ -34,6 +34,6 @@ do
 	echo "SCHED_DEADLINE"
 	./__exp-2.sh $benchmark "$weight" SCHED_DEADLINE=1 "dl-exp2"
 
-	echo "SCHED_HEARTBEAT"
-	./__exp-2.sh $benchmark "$weight" SCHED_HEARTBEAT=1 "hb-exp2"
+	# echo "SCHED_HEARTBEAT"
+	# ./__exp-2.sh $benchmark "$weight" SCHED_HEARTBEAT=1 "hb-exp2"
 done
