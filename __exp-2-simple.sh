f451c5cf3f526c5aea5efee8a8b1659f96a37eec
Kevin Zhao <kzhao@uchicago.edu>
2018-01-08 19:17:40 -0600

    .

--- __exp-2-simple.sh
+++ __exp-2-simple.sh
@@ -4,5 +4,21 @@ name=$1
 weights=$2
 schedtype=$3
 
+if [ $schedtype == "SCHED_DEADLINE=1" ]
+then
+	cd $cpuset_dir
+	echo 0 > cpuset.sched_load_balance
+	echo $$ > dl-cpuset/tasks
+	cd - > /dev/null
+fi
+
 sudo exp_nr=2 weights="$weights" $schedtype\
      ./bin/parsecmgmt -c gcc-hooks -a run -p $name -n 4 -i native
+
+if [ $schedtype == "SCHED_DEADLINE=1" ]
+then
+	cd $cpuset_dir
+	echo $$ > tasks
+	echo 1 > cpuset.sched_load_balance
+	cd - > /dev/null
+fi
